c1728d940abac3735100a8eb5466d4794f9d05f9
nanzong <iszong@yeah.net>
2022-04-06 19:11:16 +0800

    dev3 代码提交

---
+++ app3/src/main/java/cn/my/app3/MainActivity.kt
@@ -0,0 +1,11 @@
+package cn.my.app3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
